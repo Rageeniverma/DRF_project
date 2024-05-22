e1fc6e6cf2c78b22d5bf387e7e2bf27adf986e79
Rageeniverma <114106713+Rageeniverma@users.noreply.github.com>
2024-05-22 23:28:40 +0530

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class Car(models.Model):
-    car_name = models.CharField(max_length=100)
-    car_version = models.CharField(max_length=3)
-    car_model = models.CharField(max_length=30)
